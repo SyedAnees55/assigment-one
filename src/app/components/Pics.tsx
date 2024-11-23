a94723f986d47964a7d12e7856d7cf4f17577ad6
syed Anees <SyedAnees55>
2024-11-24 02:27:45 +0500

    first commit

---
+++ src/app/components/Pics.tsx
@@ -0,0 +1,62 @@
+
+
+function Pic(){
+  return (
+    <main className="lg:flex md:flex-col lg:flex-row">
+      <div className="flex lg:gap-5 md:gap-5 md:mt-5 sm:flex-col md:flex-row">    <div className="border-black border-4 w-3/12 text-center p-4">
+      {/* pics four */}
+      <img src="/R.png" 
+      alt="hello" 
+      width={300}
+       height={300}  className="justify-center "/>
+       <h1 className="m-2">Perfume</h1>
+       <p className="m-2">RS:-500</p>
+       <button
+        className="border-green-400
+        bg-green-300 
+        border-2 w-6/12">Buy</button>
+       </div>
+         <div className="border-black border-4 w-3/12 text-center p-4">
+      {/* pics four */}
+      <img src="/R.png" 
+      alt="hello" 
+      width={300}
+       height={300}  className="justify-center "/>
+       <h1 className="m-2">Perfume</h1>
+       <p className="m-2">RS:-500</p>
+       <button
+        className="border-green-400
+        bg-green-300 
+        border-2 w-6/12">Buy</button>
+       </div>
+       </div>
+  <div className="flex lg:gap-5 md:gap-5 md:mt-5 md:flex-row sm:flex-col">    <div className="border-black border-4 w-3/12 text-center p-4">
+      {/* pics four */}
+      <img src="/R.png" 
+      alt="hello" 
+      width={300}
+       height={300}  className="justify-center "/>
+       <h1 className="m-2">Perfume</h1>
+       <p className="m-2">RS:-500</p>
+       <button
+        className="border-green-400
+        bg-green-300 
+        border-2 w-6/12">Buy</button>
+       </div>
+         <div className="border-black border-4 w-3/12 text-center p-4">
+      {/* pics four */}
+      <img src="/R.png" 
+      alt="hello" 
+      width={300}
+       height={300}  className="justify-center "/>
+       <h1 className="m-2">Perfume</h1>
+       <p className="m-2">RS:-500</p>
+       <button
+        className="border-green-400
+        bg-green-300 
+        border-2 w-6/12">Buy</button>
+       </div>
+       </div>
+    </main>
+  );}
+  export default Pic;
