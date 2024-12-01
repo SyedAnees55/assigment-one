52bf3fcc75df97e9aa7a6cdace4846b272be6d16
syed Anees <SyedAnees55>
2024-12-01 11:53:12 +0500

    first commit

--- src/app/components/Pics.tsx
+++ src/app/components/Pics.tsx
@@ -1,62 +1,87 @@
+import React from 'react'
 
-
-function Pic(){
+const Pics = () => {
   return (
-    <main className="lg:flex md:flex-col lg:flex-row">
-      <div className="flex lg:gap-5 md:gap-5 md:mt-5 sm:flex-col md:flex-row">    <div className="border-black border-4 w-3/12 text-center p-4">
-      {/* pics four */}
-      <img src="/R.png" 
-      alt="hello" 
-      width={300}
-       height={300}  className="justify-center "/>
-       <h1 className="m-2">Perfume</h1>
-       <p className="m-2">RS:-500</p>
-       <button
-        className="border-green-400
+    <div className="lg:flex lg:flex-row lg:gap-5 md:grid md:grid-cols-2  flex flex-col items-center gap-5">
+      <div className="border-black border-4 w-3/12 text-center p-4 md:w-60">
+        {/* pics one */}
+        <img
+          src="/R.png"
+          alt="hello"
+          width={300}
+          height={300}
+          className="justify-center "
+        />
+        <h1 className="m-2">Perfume</h1>
+        <p className="m-2">RS:-500</p>
+        <button
+          className="border-green-400
         bg-green-300 
-        border-2 w-6/12">Buy</button>
-       </div>
-         <div className="border-black border-4 w-3/12 text-center p-4">
-      {/* pics four */}
-      <img src="/R.png" 
-      alt="hello" 
-      width={300}
-       height={300}  className="justify-center "/>
-       <h1 className="m-2">Perfume</h1>
-       <p className="m-2">RS:-500</p>
-       <button
-        className="border-green-400
+        border-2 w-6/12"
+        >
+          Buy
+        </button>
+      </div>
+      <div className="border-black border-4 w-3/12 text-center p-4 md:w-60">
+        {/* pics two */}
+        <img
+          src="/R.png"
+          alt="hello"
+          width={300}
+          height={300}
+          className="justify-center "
+        />
+        <h1 className="m-2">Perfume</h1>
+        <p className="m-2">RS:-500</p>
+        <button
+          className="border-green-400
         bg-green-300 
-        border-2 w-6/12">Buy</button>
-       </div>
-       </div>
-  <div className="flex lg:gap-5 md:gap-5 md:mt-5 md:flex-row sm:flex-col">    <div className="border-black border-4 w-3/12 text-center p-4">
-      {/* pics four */}
-      <img src="/R.png" 
-      alt="hello" 
-      width={300}
-       height={300}  className="justify-center "/>
-       <h1 className="m-2">Perfume</h1>
-       <p className="m-2">RS:-500</p>
-       <button
-        className="border-green-400
+        border-2 w-6/12"
+        >
+          Buy
+        </button>
+      </div>
+      <div className="border-black border-4 w-3/12 text-center p-4 md:w-60">
+        {/* pics three */}
+        <img
+          src="/R.png"
+          alt="hello"
+          width={300}
+          height={300}
+          className="justify-center"
+        />
+        <h1 className="m-2">Perfume</h1>
+        <p className="m-2">RS:-500</p>
+        <button
+          className="border-green-400
         bg-green-300 
-        border-2 w-6/12">Buy</button>
-       </div>
-         <div className="border-black border-4 w-3/12 text-center p-4">
-      {/* pics four */}
-      <img src="/R.png" 
-      alt="hello" 
-      width={300}
-       height={300}  className="justify-center "/>
-       <h1 className="m-2">Perfume</h1>
-       <p className="m-2">RS:-500</p>
-       <button
-        className="border-green-400
+        border-2 w-6/12"
+        >
+          Buy
+        </button>
+      </div>
+      <div className="border-black border-4 w-3/12 text-center p-4 md:w-60">
+        {/* pics four */}
+        <img
+          src="/R.png"
+          alt="hello"
+          width={300}
+          height={300}
+          className="justify-center "
+        />
+        <h1 className="m-2">Perfume</h1>
+        <p className="m-2">RS:-500</p>
+        <button
+          className="border-green-400
         bg-green-300 
-        border-2 w-6/12">Buy</button>
-       </div>
-       </div>
-    </main>
-  );}
-  export default Pic;
+        border-2 w-6/12"
+        >
+          Buy
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Pics;
+ 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Pic from "./components/Pics"
 
 export default function Home (){
     return(
-        <>
+        <div>
+            <h1 className="bg-black text-center text-xl p-3 mb-4 text-white">Assigment-9</h1>
         <Pic />
-        </>
+        </div>
     )
 }
